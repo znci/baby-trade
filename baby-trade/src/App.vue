d6c4902de8e29ab68c31e2c1fcc8baf2bdad1363
zNotChill <znotchill@znci.dev>
2024-02-24 20:06:01 +0000

    beans

---
+++ baby-trade/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Header from './components/Header.vue';
+import Sidebar from './components/Sidebar.vue';
+</script>
+
+<template>
+  <div class="main-ui">
+    <Header />
+    <Sidebar />
+  </div>
+  <div class="game-ui">
+    <h1>Inventory</h1>
+
+  </div>
+</template>
---
+++ baby-trade/src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <header class="header">
+    <div class="left-side">
+      <div class="container align-center">
+        <div class="container-content">
+          Baby Trade
+        </div>
+      </div>
+      <div class="container align-center justify-center">
+        <div class="container-content">
+          <span class="dollar-sign">$</span>100M &bull; 95 Babies
+        </div>
+      </div>
+    </div>
+    <div class="right-side">
+      <div class="container small-padding large">
+        <div class="container-content news-box fixed">
+          <div class="container-title">
+            Recent News
+          </div>
+          <div class="small js-news-box"></div>
+        </div>
+      </div>
+      <div class="container symmetrical justify-center align-center">
+        <div class="container-content">
+          <div class="container-icon">
+            cool icon here
+          </div>
+          <div class="center js-clock-date">
+            12 Aug
+          </div>
+          <div class="center small js-clock-time">17:30</div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
